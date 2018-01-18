e3ca23bc14afd5a120af1d1e0525fb786b325958
ad3adp03t <ad3adp03t@gmail.com>
2018-01-17 20:02:20 -0500

    Combined both functions into one. Made so array excluded top number. Wrote funciton to add all values in array of multiples together

--- euler-problem-1/app.rb
+++ euler-problem-1/app.rb
@@ -2,26 +2,22 @@
 
 # Find the sum of all the multiples of 3 or 5 below 1000.
 @mult_3and5 = Array.new
-def multiple3 (num)
-    arr = *(1..num)
+def multiple3and5(num)
+    arr = *(1...num)
     arr.each do |x|
         if x % 3 == 0
             @mult_3and5<< x
+        elsif x % 5 == 0  
+            @mult_3and5<< x  
         end
     end
     puts @mult_3and5
+    add_multiples
 end
 
-multiple3(10)
-            
-def multiple5 (num)
-    arr = *(1..num)
-    arr.each do |x|
-        if x % 5 == 0
-            @mult_3and5<< x
-        end
-    end
+def add_multiples
+    @mult_3and5 = @mult_3and5.inject(0){|sum,x| sum + x }
     puts @mult_3and5
-end         
+end    
 
-multiple5(10)
+multiple3and5(10)
