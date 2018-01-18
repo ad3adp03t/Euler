04b1665dc0514c8f39b640dbd14503a820b059b9
ad3adp03t <ad3adp03t@gmail.com>
2018-01-17 19:50:57 -0500

    Wrote function for multiples of 10.
    Made multiples from both functions pass into same array

--- euler-problem-1/app.rb
+++ euler-problem-1/app.rb
@@ -1,17 +1,27 @@
 # If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
 # Find the sum of all the multiples of 3 or 5 below 1000.
-
+@mult_3and5 = Array.new
 def multiple3 (num)
-    mult_3 = Array.new
     arr = *(1..num)
     arr.each do |x|
         if x % 3 == 0
-            mult_3<< x
+            @mult_3and5<< x
         end
     end
-    puts mult_3
+    puts @mult_3and5
 end
 
 multiple3(10)
             
+def multiple5 (num)
+    arr = *(1..num)
+    arr.each do |x|
+        if x % 5 == 0
+            @mult_3and5<< x
+        end
+    end
+    puts @mult_3and5
+end         
+
+multiple5(10)
