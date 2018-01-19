327825b90f084ae92d32e42c01a3e2cc542d499e
ad3adp03t <ad3adp03t@gmail.com>
2018-01-18 23:44:07 -0500

    required "prime" and rewrote to check for first "num" many primes

--- euler-problem-7/app.rb
+++ euler-problem-7/app.rb
@@ -1,3 +1,4 @@
+require "prime"
 # Problem 7 
 # By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 
@@ -5,18 +6,7 @@
 
 
 def prime_generator(num)
-    primes = [2]
-    count = 3
-    loop do 
-        if count%2 === 0
-        else 
-            primes<<count
-        end
-        count+=1
-        if count == num
-            break
-        end    
-    end 
+    primes = Prime.first(num)
     p primes
 end
 
