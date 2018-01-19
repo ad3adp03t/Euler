f302414124428c0b26ec1535a309a8f78ebbe777
ad3adp03t <ad3adp03t@gmail.com>
2018-01-18 23:11:41 -0500

    required "prime" made function turn factors into multidimensional array did testing to make sure where largest value returns

--- euler-problem-3/app.rb
+++ euler-problem-3/app.rb
@@ -1,13 +1,16 @@
+require 'prime'
 # Problem 3 
 # The prime factors of 13195 are 5, 7, 13 and 29.
 
 # What is the largest prime factor of the number 600851475143 ?
 
 def prime_factors(num)
-    factors=Array.new
-    count = 3
-    loop do |x|
-        x = num/count
+    factors = num.prime_division
+    factors.to_a
+    largest_factor = factors[-1][0]
+    p largest_factor
+end    
 
+prime_factors(131956)
         
         
